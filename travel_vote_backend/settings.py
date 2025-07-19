141bc92a161bf53e02b5294361246e7a81375e40
kth4778 <cnblue123429@gmail.com>
2025-07-20 02:53:02 +0900

    "Add Vercel frontend domain to CORS_ALLOWED_ORIGINS"

--- travel_vote_backend/settings.py
+++ travel_vote_backend/settings.py
@@ -134,6 +134,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React 개발 서버
     "http://127.0.0.1:3000",
+    "https://travel-vote-app-project.vercel.app", # Vercel 프론트엔드 도메인 추가
 ]
 
 CORS_ALLOW_CREDENTIALS = True
